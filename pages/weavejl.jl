34265386830e0d2e6c975b2b82226aa5f0c0cae1
rmsrosa <rmsrosa@gmail.com>
2022-02-13 01:56:21 +0000

    Deploying to gh-pages from @ rmsrosa/booksjl-franklin-template@2f129c7c1c9b65a926590dbc89d25985721666ec 🚀

---
+++ pages/weavejl.jl
@@ -0,0 +1,9 @@
+#' # Weaved Julia Script
+
+#' This page was generated from a **julia script**, automatically converted to markdown with [Weave.jl](https://github.com/JunoLab/Weave.jl). The conversion happens when the menu is constructed.
+
+#' ## Julia chunk
+
+#' Here is a julia chunk:
+
+x = 2
