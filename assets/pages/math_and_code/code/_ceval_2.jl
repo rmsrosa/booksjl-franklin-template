887a29736849a2d89c5404b0768b358de5a5c00d
rmsrosa <rmsrosa@gmail.com>
2022-02-14 22:40:05 +0000

    Deploying to gh-pages from @ rmsrosa/booksjl-franklin-template@01dc2f46d54d6ea54bbd124f4167e809b3211c03 🚀

--- assets/pages/math_and_code/code/_ceval_2.jl
+++ assets/pages/math_and_code/code/_ceval_2.jl
@@ -1,4 +1,3 @@
 # This file was generated, do not modify it. # hide
 import Pkg
 Pkg.activate("pages")
-using Plots
--- assets/pages/math_and_code/code/ex_show.jl
+++ assets/pages/math_and_code/code/ex_show.jl
@@ -1,5 +1,3 @@
 # This file was generated, do not modify it. # hide
 x = 0.0:0.01:2pi
-y = sin.(2x) + sin.(5x)
-plot(x, y)
-savefig(joinpath(@OUTPUT, "sin.svg")) # hide
+y = sin.(2x) + sin.(5x);
