45c8106d6e91a61d1f521af6e428e407ccf55567
rmsrosa <rmsrosa@gmail.com>
2022-02-14 23:15:59 +0000

    Deploying to gh-pages from @ rmsrosa/booksjl-franklin-template@c1ed6b80c46ab43995211c18175e99c384e188ae 🚀

--- assets/pages/math_and_code/code/_ceval_2.jl
+++
@@ -1,3 +0,0 @@
-# This file was generated, do not modify it. # hide
-import Pkg
-Pkg.activate("pages")
--- assets/pages/math_and_code/code/_ceval_3.jl
+++
@@ -1,3 +0,0 @@
-# This file was generated, do not modify it. # hide
-x = 0.0:0.01:2pi
-y = sin.(2x) + sin.(5x);
