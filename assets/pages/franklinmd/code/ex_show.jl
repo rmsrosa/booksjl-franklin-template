a6afe15c4d654bb1c8a99cbf910b8279833a3ed3
rmsrosa <rmsrosa@gmail.com>
2022-03-15 11:10:38 +0000

    Deploying to gh-pages from @ rmsrosa/booksjl-franklin-template@497a9bafcc3d094f790be0e903e2126ab86d5d6e 🚀

--- assets/pages/franklinmd/code/ex_show.jl
+++ assets/pages/franklinmd/code/ex_show.jl
@@ -1,5 +1,5 @@
 # This file was generated, do not modify it. # hide
 x = 0.0:0.01:2π
 y = sin.(2x) + sin.(5x)
-plot(x, y)
+plot(x, y, title = "A nice wave", titlefont=12, label = false)
 savefig(joinpath(@OUTPUT, "sin.svg")) # hide
